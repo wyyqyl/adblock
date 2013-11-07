1f5e7549f80fad1fc4b17de108d9ac2f55cd6636
yorath <yorathwang@gmail.com>
2013-11-07 17:10:44 +0800

    add JavaScript debug support

--- src/adblock_impl.cpp
+++ src/adblock_impl.cpp
@@ -2,11 +2,40 @@
 #include "env.h"
 #include "js_object.h"
 
+#ifdef ENABLE_DEBUGGER_SUPPORT
+#include <v8-debug.h>
+#endif  // ENABLE_DEBUGGER_SUPPORT
+
 #include <boost/bind.hpp>
 
 namespace adblock {
 
 extern std::string js_sources[];
+
+#ifdef ENABLE_DEBUGGER_SUPPORT
+v8::Persistent<v8::Context> debug_message_context;
+
+void DispatchDebugMessages() {
+  // We are in some random thread. We should already have v8::Locker acquired
+  // (we requested this when registered this callback). We was called
+  // because new debug messages arrived; they may have already been processed,
+  // but we shouldn't worry about this.
+  //
+  // All we have to do is to set context and call ProcessDebugMessages.
+  //
+  // We should decide which V8 context to use here. This is important for
+  // "evaluate" command, because it must be executed some context.
+  // In our sample we have only one context, so there is nothing really to
+  // think about.
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context =
+    v8::Local<v8::Context>::New(isolate, debug_message_context);
+  v8::Context::Scope scope(context);
+
+  v8::Debug::ProcessDebugMessages();
+}
+#endif  // ENABLE_DEBUGGER_SUPPORT
 
 void CreateInstance(AdBlockPtr *adblock) {
   *adblock = nullptr;
@@ -43,9 +72,23 @@ bool AdBlockImpl::Init() {
 
     env_->SetEventCallback("init",
                            boost::bind(&AdBlockImpl::InitDone, this, _1));
+#ifdef ENABLE_DEBUGGER_SUPPORT
+    debug_message_context.Reset(isolate, context);
+    v8::Debug::SetDebugMessageDispatchHandler(DispatchDebugMessages, true);
+    v8::Debug::EnableAgent("adblock", 9222, false);
+
+    getchar();
+#endif  // ENABLE_DEBUGGER_SUPPORT
+
     for (int idx = 0; !js_sources[idx].empty(); idx += 2) {
       env_->Evaluate(js_sources[idx + 1], js_sources[idx]);
     }
+
+    auto fun_name = v8::String::NewFromUtf8(isolate, "initAdblock");
+    auto process_val = context->Global()->Get(fun_name);
+    auto func = JsValuePtr(new JsValue(isolate, process_val));
+    func->Call();
+
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
     return false;
