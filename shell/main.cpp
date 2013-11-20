d8e5bddb07b8218bf4032caffaa04447eb8793bd
yorath <yorathwang@gmail.com>
2013-11-20 16:01:21 +0800

    build all the libs into one executable

--- shell/main.cpp
+++ shell/main.cpp
@@ -5,6 +5,29 @@
 #include <vld.h>
 #include <ctime>
 
+#pragma comment(lib, "Winmm.lib")
+#pragma comment(lib, "Wldap32.lib")
+#pragma comment(lib, "Ws2_32.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "v8_snapshot-sd.lib")
+#pragma comment(lib, "v8_base.ia32-sd.lib")
+#pragma comment(lib, "icuuc-sd.lib")
+#pragma comment(lib, "icui18n-sd.lib")
+#pragma comment(lib, "libcurl-sd.lib")
+#pragma comment(lib, "libeay32-sd.lib")
+#pragma comment(lib, "ssleay32-sd.lib")
+#pragma comment(lib, "zlib-sd.lib")
+#else
+#pragma comment(lib, "v8_snapshot-s.lib")
+#pragma comment(lib, "v8_base.ia32-s.lib")
+#pragma comment(lib, "icuuc-s.lib")
+#pragma comment(lib, "icui18n-s.lib")
+#pragma comment(lib, "libcurl-s.lib")
+#pragma comment(lib, "libeay32-s.lib")
+#pragma comment(lib, "ssleay32-s.lib")
+#pragma comment(lib, "zlib-s.lib")
+#endif
+
 int main() {
   try {
     adblock::AdBlockPtr adblock;
@@ -14,10 +37,14 @@ int main() {
     }
 
     time_t timer = time(nullptr);
-    adblock->CheckFilterMatch("http://pagead2.googlesyndication.com/pagead/show_ads.js", "SCRIPT", "http://bbs.pediy.com/");
+    adblock->CheckFilterMatch(
+        "http://pagead2.googlesyndication.com/pagead/show_ads.js", "SCRIPT",
+        "http://bbs.pediy.com/");
     adblock->GetElementHidingSelectors("http://reddit.com/");
-    std::cout << "Time elapsed: " << difftime(time(nullptr), timer) << std::endl;
-  } catch (const std::exception& e) {
+    std::cout << "Time elapsed: " << difftime(time(nullptr), timer)
+              << std::endl;
+  }
+  catch (const std::exception& e) {
     OutputDebugStringA(e.what());
     std::cerr << e.what() << std::endl;
   }
