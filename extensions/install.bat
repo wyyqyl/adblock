69b18f51ccb1401b883038b80b10fee6f033faae
yorath <yorathwang@gmail.com>
2013-12-26 17:00:16 +0800

    remove bat

--- extensions/install.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-reg add "HKLM\Software\Wow6432Node\Google\Chrome\Extensions\ikoecccpabgkgdjemjjfkcmpjhchnalh" /f /v path /t REG_SZ /d "%~dp0chrome.crx"
-reg add "HKLM\Software\Wow6432Node\Google\Chrome\Extensions\ikoecccpabgkgdjemjjfkcmpjhchnalh" /f /v version /t REG_SZ /d "1.0"
--- extensions/uninstall.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-reg delete "HKLM\Software\Wow6432Node\Google\Chrome\Extensions\ikoecccpabgkgdjemjjfkcmpjhchnalh" /f 
